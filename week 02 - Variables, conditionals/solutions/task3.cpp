55f0a72ae6330a23bc56f81daa493ddf6c9938e2
Nikola Nikolov <nikola.m.nikolov314@gmail.com>
2023-10-18 20:49:09 +0300

    fix task3 solution

--- week 02 - Variables, conditionals/solutions/task3.cpp
+++ week 02 - Variables, conditionals/solutions/task3.cpp
@@ -9,13 +9,15 @@ int main() {
   std::cin >> c;
 
 
-  bool is_digit = c >= '0' && c <= '9';
-
-  if (is_digit) {
+  if (c >= '0' && c <= '9') {
     std::cout << "THE SYMBOL IS A DIGIT" << std::endl;
   }
+  else if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+    std::cout << "THE SYMBOL IS A LETTER" << std::endl;
+  }
+
   else {
-    std::cout << "THE SYMBOL IS NOT A DIGIT" << std::endl;
+    std::cout << "THE SYMBOL IS NEITHER A LETTER NOR A DIGIT" << std::endl;
   }
 
   return 0;
